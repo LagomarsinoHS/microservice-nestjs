c31bbc3d0c421c0d0c3d807c36afbfee61f1dfef
Humberto Lagomarsino <h.lagomarsino.g@gmail.com>
2022-03-30 17:30:52 -0300

    Delete principal/dist directory

--- principal/dist/entities/product.model.d.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from "mongoose";
-export declare type ProductDocument = ProductModelEntity & Document;
-export declare class ProductModelEntity {
-    product_id: number;
-    title: string;
-    image: string;
-    likes: number;
-}
-export declare const ProductSchema: any;
--- principal/dist/modules/product/product.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { ProductService } from './product.service';
-export declare class ProductController {
-    private readonly productService;
-    constructor(productService: ProductService);
-    getAllProducts(): unknown;
-    hello(data: string): any;
-    createProduct(data: string): unknown;
-}
